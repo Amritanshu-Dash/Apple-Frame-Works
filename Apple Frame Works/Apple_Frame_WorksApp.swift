64f9e5f37a6b62b53d94c853392fbe6265a0bd29
Amritanshu Dash <amritanshudash6677@gmail.com>
2024-05-30 14:49:34 +0530

    Initial Commit

---
+++ Apple Frame Works/Apple_Frame_WorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Frame_WorksApp.swift
+//  Apple Frame Works
+//
+//  Created by Amritanshu Dash on 30/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Frame_WorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
